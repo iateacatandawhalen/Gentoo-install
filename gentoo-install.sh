6163f71c2337cbe728f1189ae0e071d080178e98
iateacatandawhalen <96869531+iateacatandawhalen@users.noreply.github.com>
2025-03-08 08:58:12 +0100

    Update gentoo-install.sh

--- gentoo-install.sh
+++ gentoo-install.sh
@@ -4,6 +4,8 @@
 DISK="/dev/sda"    # Target disk
 ROOT_PARTITION="/dev/sda1"
 SWAP_PARTITION="/dev/sda2"
+BOOT_PARTITION="/dev/sda3"
+LIMINE_CONF="/mnt/gentoo/etc/limine/limine.conf"
 
 # Step 1: Partition the disk (Modify as necessary)
 echo "Creating partitions on $DISK..."
@@ -29,7 +31,6 @@ mirror_url="http://distfiles.gentoo.org"
 emerge --sync
 
 # Install the basic Gentoo system using stage3 tarball
-# This step may take a while
 wget $mirror_url/releases/amd64/autobuilds/20250304T214502Z/stage3-amd64-20250304T214502Z.tar.xz -P /mnt/gentoo
 tar xpvf /mnt/gentoo/stage3-amd64-*.tar.xz --xattrs --numeric-owner -C /mnt/gentoo
 
@@ -43,9 +44,30 @@ cp /etc/resolv.conf /mnt/gentoo/etc/resolv.conf
 
 chroot /mnt/gentoo /bin/bash
 
-# Step 6: Set up the Portage tree
+# Step 6: Set up Portage properly for Intel Celeron 2nd Gen
 echo "Setting up Portage..."
-emerge-webrsync
+
+# Sync Portage tree (useful to get the latest package information)
+emerge --sync
+
+# Set up the make.conf file (you can adjust CFLAGS and USE flags here)
+echo "Setting up make.conf..."
+
+# CFLAGS, CXXFLAGS for Intel Celeron 2nd Gen optimization
+cat > /etc/portage/make.conf <<EOL
+# Set CFLAGS for Intel Celeron 2nd Gen (Core 2 architecture)
+CFLAGS="-O2 -march=core2 -pipe"
+CXXFLAGS="${CFLAGS}"
+
+# Set USE flags (you can adjust based on your needs)
+USE="X"
+
+# Makeopts for parallel builds based on the number of cores
+MAKEOPTS="-j$(nproc)"
+EOL
+
+# Set the appropriate profile (change if you need a different profile)
+eselect profile set default/linux/amd64/17.1
 
 # Step 7: Install essential packages
 echo "Installing essential packages..."
@@ -63,7 +85,25 @@ emerge --ask sys-boot/limine
 # Configure Limine
 limine-install /mnt/gentoo
 
-# Step 9: Configure the system
+# Step 9: Create the Limine configuration file as a variable
+LIMINE_CONFIG_CONTENT=$(cat <<EOL
+# Limine bootloader config file for Gentoo
+default gentoo
+prompt 0
+timeout 50
+
+# Boot the Gentoo kernel
+label gentoo
+  kernel /boot/vmlinuz
+  append initrd=/boot/initramfs.img root=/dev/sda1
+  ramdisk_size=8192
+EOL
+)
+
+# Step 10: Write the Limine config to the correct location
+echo "$LIMINE_CONFIG_CONTENT" > /mnt/gentoo$LIMINE_CONF
+
+# Step 11: Configure the system
 echo "Configuring system..."
 
 # Set the timezone to Europe/Copenhagen
@@ -76,22 +116,23 @@ echo "hostname=\"gentoo\"" > /mnt/gentoo/etc/conf.d/hostname
 echo "config_eth0=\"dhcp\"" > /mnt/gentoo/etc/conf.d/net
 rc-update add net.eth0 default
 
-# Step 10: Create fstab
+# Step 12: Create fstab
 echo "Creating fstab..."
 genfstab -U /mnt/gentoo >> /mnt/gentoo/etc/fstab
 
-# Step 11: Set the root password
+# Step 13: Set the root password
 echo "Setting root password..."
 passwd
 
-# Step 12: Exit chroot and unmount
+# Step 14: Exit chroot and unmount
 echo "Exiting chroot..."
 exit
 
-# Step 13: Unmount the filesystems
+# Step 15: Unmount the filesystems
 echo "Unmounting..."
 umount -R /mnt/gentoo
 
-# Step 14: Reboot
+# Step 16: Reboot
 echo "Installation complete! Rebooting..."
 reboot
+
